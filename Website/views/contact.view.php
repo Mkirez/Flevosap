e117dc2f4888dedec1721826e1a60fe5b6de450a
Alex <AlexSteen303@gmail.com>
2020-10-07 18:19:04 +0200

    contact pagina. Met mail mogelijkheid en gekaderd contactinfo.

--- Website/views/contact.view.php
+++ Website/views/contact.view.php
@@ -4,15 +4,43 @@
 <?php include "includes/head.view.php" ?>
 <body>
 <?php include "includes/nav.view.php" ?>
-<h3 style="text-align: left;">Neem contact op met FlevoSap:</h3>
+<br>
+<h3 style="text-align: left;" class="col-md-6">Neem contact op met FlevoSap:</h3>
 <section class="body">
-    <p style="text-align: left;">
-        <br>Flevosap bv
-        <br>Prof. Zuurlaan 22
-        <br>8256 PE Biddinghuizen, Nederland
-        <br>Tel: +31 (0)321 – 33 25 25
-        <br>info@flevosap.nl
-    </p>
+    <div class="col-md-6">
+        <div class="inner" id="inner-form">
+            <form action="sendmail.php" method="POST">
+                <br>
+                <div class="form-group name-group">
+                    <input name="naam" type="text" class="form-control form-control-sm"  id="loremipsum" placeholder="naam">
+                </div>
+                <div class="form-group email-group">
+                    <input type="text" name="onderwerp" class="form-control form-control-sm"  id="loremipsum" 	placeholder="onderwerp">
+                </div>
+                <div class="form-group achternaam-group">
+                    <input type="text" class="form-control form-control-sm" id="loremipsum" name="email" placeholder="email">
+                </div>
+                <div class="form-group">
+                    <textarea name="bericht" class="form-control" id="loremipsum" rows="3"></textarea>
+                </div>
+                <button type="submit" name="submit" class="btn btn-primary" placeholder="lorem ipsum">Submit</button>
+                <p>
+                    <br>
+                </p>
+            </form>
+        </div>
+    </div>
+    <div class="form-group">
+        <p style="text-align: left"; class="col-md-6";>
+            <textarea name="myTextBox" cols="36" rows="6" style="border:3px solid #bd2130;">
+Flevosap bv
+Prof. Zuurlaan 22
+8256 PE Biddinghuizen, Nederland
+Tel: +31 (0)321 – 33 25 25
+info@flevosap.nl
+            </textarea>
+        </p>
+    </div>
 </section>
 <?php include "includes/footer.view.php" ?>
 </body>
