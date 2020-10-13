a57970c38fe7ff4bb4bf33fa0a80f3c3289088eb
lvvcolin <lorenzo8399@hotmail.com>
2020-10-13 13:54:08 +0200

    home page

--- Website/views/includes/nav.view.php
+++ Website/views/includes/nav.view.php
@@ -2,13 +2,17 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12 no-padding">
-                <nav class="navbar navbar-expand-lg navbar-light navbar-color">
+                <nav class="navbar navbar-expand-lg navbar-light bg-color-light">
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                        <div class="mx-auto">
-                            <a href="/" class="navbar-brand">Flevosap</a>
+                        <div class="mx-auto logo-header">
+                            <a href="">
+                                <img src="../../asset/img/logo-headerV3.png" >
+                            </a>
+                            
+
                         </div>
                         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                             <li class="nav-item active">
