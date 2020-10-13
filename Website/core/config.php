62ad2371f0b935b2d647caf10d53d97bdb238fbf
Diamé Morreau <diamemorreau@me.com>
2020-10-13 15:50:47 +0200

    dashoard alleen bij login zichtbaar

--- Website/core/config.php
+++ Website/core/config.php
@@ -2,7 +2,12 @@
 
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
+<<<<<<< Updated upstream
 define('DB_PASSWORD', 'Welcome123');
 define('DB_NAME', 'Flevosapproducts');
 
 
+=======
+define('DB_PASSWORD', 'root');
+define('DB_NAME', 'flevodb');
+>>>>>>> Stashed changes
