8ffcbdf297d6d372b38d33e6dffd4829ef62068a
Diamé Morreau <diamemorreau@me.com>
2020-10-13 17:02:05 +0200

    admin_diame teruggezet

---
+++ Misc/admin_diame/views/includes/footer.view.php
@@ -0,0 +1,56 @@
+<div id="footer">
+<ul class="nav text-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 ">
+                <h4>Flevosap</h4>
+                <li class="nav-item">
+                    <a class="nav-link active" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Boomgaard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Veelgestelde vragen</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </div>
+            <div class="col-md-3 ">
+                <h4>Horeca</h4>
+                <li class="nav-item">
+                    <a class="nav-link active" href="/">Klant worden</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Bestellen via Flevosap.nl </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Onze Partners</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Nieuwsbrief aanvragen</a>
+                </li>
+            </div>
+            <div class="col-md-3 ">
+                <h4>Contact</h4>
+                <li class="nav-item">
+                    <a class="nav-link active" href="/">Contact</a>
+                </li>
+            </div>
+            <div class="col-md-3 ">
+                <h4>Volgs ons op</h4>
+                <li class="nav-item">
+                    <a class="nav-link active" href="https://www.facebook.com/Flevosap"><i class="fa fa-facebook"></i>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://www.linkedin.com/company/vermeulen-food-group/"><i class="fa fa-instagram"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://www.instagram.com/flevosap/?hl=nl"><i class="fa fa-linkedin"></i></a>
+                </li>
+            </div>
+        </div>
+    </div>
+</ul>
+</div>
---
+++ Misc/admin_diame/views/includes/nav.view.php
@@ -0,0 +1,10 @@
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 no-padding">
+            <li class="nav-item">
+                <a class="nav-link" href="/logout">Logout</a>
+            </li>
+            </div>
+        </div>
+    </div>
