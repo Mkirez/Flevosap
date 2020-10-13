eca484dfa6d423d73dd6e7c1851e02ac74280304
Alex <AlexSteen303@gmail.com>
2020-10-13 17:21:12 +0200

    Admin_Prototype aangepast. Alleen noodzakelijke erin

--- Misc/Admin_Prototype/index.php
+++ Misc/Admin_Prototype/index.php
@@ -26,16 +26,11 @@ require 'models/ProductInfoModel.php';
  * Controllers
  */
 require 'controllers/HomeController.php';
-require 'controllers/AboutController.php';
 require 'controllers/AdminController.php';
-require 'controllers/ContactController.php';
-require 'controllers/RegisterController.php';
 require 'controllers/LoginController.php';
 require 'controllers/LogoutController.php';
 require 'controllers/AdminLoginController.php';
 require 'controllers/AdminLogoutController.php';
-require 'controllers/ProductpageController.php';
-require 'controllers/ProductInfoController.php';
 
 require 'core/Router.php';
 
