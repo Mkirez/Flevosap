ee49033e98292e69737735fb20d5e5a13d924c23
Alex <AlexSteen303@gmail.com>
2020-10-16 23:56:01 +0200

    Update views. 1 keer tabelheaders, vorige versie deed headers bij elke foreach functie. Alles zo veel ordelijker.

--- Misc/admin_diame/views/products.view.php
+++ Misc/admin_diame/views/products.view.php
@@ -5,89 +5,97 @@
 <?php include "includes/head.view.php" ?>
 <body>
 <?php include "includes/nav.view.php" ?>
-<?php foreach ($products as $productInfo){ ?>
-    <div id="weergaveProducts">
-        <table id="example" border="1" class="table table-striped table-bordered" style="width:100%">
-            <tr>
-                <th>
-                    Product ID:
-                </th>
-                <th>
-                    Productnaam:
-                </th>
-                <th>
-                    Productbeschrijving:
-                </th>
-                <th>
-                    Prijs:
-                </th>
-                <th>
-                    Hoeveelheid:
-                </th>
-                <th>
-                    Created at:
-                </th>
-                <th>
-                    Updated at:
-                </th>
-            </tr>
-            <tr>
-                <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
-                    <?= $productInfo->getId(); ?>
-                </td>
-                <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
-                    <?= $productInfo->getTitle(); ?>
-                </td>
-                <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
-                    <?= $productInfo->getProductOmschrijving(); ?>
-                </td>
-                <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
-                    <?= $productInfo->getPrijs(); ?>
-                </td>
-                <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
-                    <?= $productInfo->getHoeveelheid(); ?>
-                </td>
-                <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
-                    <?= $productInfo->getCreatedAt(); ?>
-                </td>
-                <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
-                    <?= $productInfo->getUpdatedAt(); ?>
-                </td>
-                <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
-                    <a href="javascript:void(0);" onclick="confirmEdit('Weet je zeker dat je product #<?= $productInfo->getId();?> wilt modificeren?', '/products?recordId=<?= urlencode($productInfo->getId()); ?>');">Edit</a>
-                </td>
-                <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
-                    <a href="javascript:void(0);" onclick="confirmDelete('Weet je zeker dat je product #<?= $productInfo->getId();?> wilt verwijderen? Dit is permanent.', '/products?recordId=<?= urlencode($productInfo->getId()); ?>');">Delete</a>
-                </td>
-            </tr>
-        </table>
-        <script>
-            function confirmEdit ( message, url )
-            {
-                var confirmation = confirm ( message );
 
-                if (confirmation == true ) {
-                    window.location = url;
-                } else {
-                    return false;
-                }
-            }
-        </script>
-        <script>
-            function confirmDelete ( message, url )
-            {
-                var confirmation = confirm ( message );
+<div id="weergaveProducts">
+    <table id="example" border="1" class="table table-striped table-bordered" style="width:100%">
+        <tr>
+            <th style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                Product ID:
+            </th>
+            <th style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                Productnaam:
+            </th>
+            <th style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                Productbeschrijving:
+            </th>
+            <th style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                Prijs:
+            </th>
+            <th style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                Hoeveelheid:
+            </th>
+            <th style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                Created at:
+            </th>
+            <th style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                Updated at:
+            </th>
+            <th style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                Edit
+            </th>
+            <th style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                Delete
+            </th>
+        </tr>
+    </table>
+        <?php foreach ($products as $productInfo){ ?>
+    <table>
+        <tr>
+            <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                <?= $productInfo->getId(); ?>
+            </td>
+            <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                <?= $productInfo->getTitle(); ?>
+            </td>
+            <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                <?= $productInfo->getProductOmschrijving(); ?>
+            </td>
+            <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                <?= $productInfo->getPrijs(); ?>
+            </td>
+            <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                <?= $productInfo->getHoeveelheid(); ?>
+            </td>
+            <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                <?= $productInfo->getCreatedAt(); ?>
+            </td>
+            <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                <?= $productInfo->getUpdatedAt(); ?>
+            </td>
+            <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                <a href="javascript:void(0);"
+                   onclick="confirmEdit('Weet je zeker dat je product #<?= $productInfo->getId(); ?> wilt modificeren?', '/products?recordId=<?= urlencode($productInfo->getId()); ?>');">Edit</a>
+            </td>
+            <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                <a href="javascript:void(0);"
+                   onclick="confirmDelete('Weet je zeker dat je product #<?= $productInfo->getId(); ?> wilt verwijderen? Dit is permanent.', '/products?recordId=<?= urlencode($productInfo->getId()); ?>');">Delete</a>
+            </td>
+        </tr>
+    </table>
+    <script>
+        function confirmEdit(message, url) {
+            var confirmation = confirm(message);
 
-                if (confirmation == true ) {
-                    window.location = url;
-                } else {
-                    return false;
-                }
+            if (confirmation == true) {
+                window.location = url;
+            } else {
+                return false;
             }
-        </script>
-    </div>
+        }
+    </script>
+    <script>
+        function confirmDelete(message, url) {
+            var confirmation = confirm(message);
+
+            if (confirmation == true) {
+                window.location = url;
+            } else {
+                return false;
+            }
+        }
+    </script>
+</div>
 <?php } ?>
 </body>
 </html>
-<!-- <?php }else{header('location:/admin');} ?> -->
-
+<!-- <?php } else {header('location:/admin');} ?> -->
--- Misc/admin_diame/views/users.view.php
+++ Misc/admin_diame/views/users.view.php
@@ -5,82 +5,81 @@
 <?php include "includes/head.view.php" ?>
 <body>
 <?php include "includes/nav.view.php" ?>
-<?php foreach ($users as $userInfo){ ?>
-    <div id="weergaveUsers">
-        <form action="" method="post">
-            <table id="Usertable" border="1" class="table table-striped table-bordered" style="width:100%">
-                <tr>
-                    <th>
-                        Gebruikers:
-                    </th>
-                    <th>
-                        Wachtwoorden:
-                    </th>
-                    <th>
-                        Created at:
-                    </th>
-                    <th>
-                        Updated at:
-                    </th>
-                    <th>
-                        Edit
-                    </th>
-                    <th>
-                        Delete
-                    </th>
-                </tr>
 
-                <tr>
-                    <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
-                        <?= $userInfo->getUsername(); ?>
-                    </td>
-                    <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
-                        <?= $userInfo->getPassword(); ?>
-                    </td>
-                    <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
-                        <?= $userInfo->getCreatedAt(); ?>
-                    </td>
-                    <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
-                        <?= $userInfo->getUpdatedAt(); ?>
-                    </td>
-                    <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
-                        <a href="javascript:void(0);"
-                           onclick="confirmEdit('Weet je zeker dat je gebruiker #<?= $userInfo->getId(); ?> wilt modificeren?', '/users?recordId=<?= urlencode($userInfo->getId()); ?>');">Edit</a>
-                    </td>
-                    <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
-                        <a href="javascript:void(0);"
-                           onclick="confirmDelete('Weet je zeker dat je gebruiker #<?= $userInfo->getId(); ?> wilt verwijderen? Dit is permanent.', '/users?recordId=<?= urlencode($userInfo->getId()); ?>');">Delete</a>
-                    </td>
+<div id="weergaveUsers">
+    <form action="" method="post">
+        <table id="Usertable" border="1" class="table-sm table-striped table-bordered" style="width:100%; height:60px;">
+            <tr>
+                <th style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                    Gebruikers:
+                </th>
+                <th style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                    Wachtwoorden:
+                </th>
+                <th style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                    Created at:
+                </th>
+                <th style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                    Updated at:
+                </th>
+                <th style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                    Edit
+                </th>
+                <th style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                    Delete
+                </th>
+            </tr>
+        </table>
+            <?php foreach ($users as $userInfo){ ?>
+        <table id="UserContent" border=1 class="table-sm" style="width:100%">
+            <tr>
+                <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                    <?= $userInfo->getUsername(); ?>
+                </td>
+                <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                    <?= $userInfo->getPassword(); ?>
+                </td>
+                <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                    <?= $userInfo->getCreatedAt(); ?>
+                </td>
+                <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                    <?= $userInfo->getUpdatedAt(); ?>
+                </td>
+                <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                    <a href="javascript:void(0);"
+                       onclick="confirmEdit('Weet je zeker dat je gebruiker #<?= $userInfo->getId(); ?> wilt modificeren?', '/users?recordId=<?= urlencode($userInfo->getId()); ?>');">Edit</a>
+                </td>
+                <td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">
+                    <a href="javascript:void(0);"
+                       onclick="confirmDelete('Weet je zeker dat je gebruiker #<?= $userInfo->getId(); ?> wilt verwijderen? Dit is permanent.', '/users?recordId=<?= urlencode($userInfo->getId()); ?>');">Delete</a>
+                </td>
+            </tr>
+        </table>
+    </form>
+    <script>
+        function confirmEdit(message, url) {
+            var confirmation = confirm(message);
 
-                </tr>
-            </table>
-        </form>
-            <script>
-                function confirmEdit ( message, url )
-                {
-                    var confirmation = confirm ( message );
+            if (confirmation == true) {
+                window.location = url;
+            } else {
+                return false;
+            }
+        }
+    </script>
+    <script>
+        function confirmDelete(message, url) {
+            var confirmation = confirm(message);
 
-                    if (confirmation == true ) {
-                        window.location = url;
-                    } else {
-                        return false;
-                    }
-                }
-            </script>
-            <script>
-                function confirmDelete ( message, url )
-                {
-                    var confirmation = confirm ( message );
-
-                    if (confirmation == true ) {
-                        window.location = url;
-                    } else {
-                        return false;
-                    }
-                }
-            </script>
-    </div>
+            if (confirmation == true) {
+                window.location = url;
+            } else {
+                return false;
+            }
+        }
+    </script>
+</div>
 <?php } ?>
 </body>
 </html>
-<!-- <?php }else{header('location:/admin');}?> -->
+<!-- <?php } else {header('location:/admin');} ?> -->
