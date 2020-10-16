d7847d2ddc938c87b7f8bdd016cfee201e93e6e5
YektaU <yekta97@live.nl>
2020-10-16 12:59:10 +0200

    admin functie view test diame

--- Misc/admin_diame/views/users.view.php
+++ Misc/admin_diame/views/users.view.php
@@ -4,11 +4,40 @@
 <?php $title = "Dashboard" ?>
 <?php include "includes/head.view.php" ?>
 <body>
-    <?php include "includes/nav.view.php" ?>
-    <h3 style="text-align: left;">Dashboard</h3>
-    <?php
-    print_r($users);
-    ?>
+<?php foreach ($users as $userInfo){ ?>
+    <div id="weergaveUsers">
+        <table>
+            <tr>
+                <th>
+                    Gebruikers:
+                </th>
+                <th>
+                    Wachtwoorden:
+                </th>
+                <th>
+                    Created at:
+                </th>
+                <th>
+                    Updated at:
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    <?php $userInfo["gebruikersnaam"] ?>
+                </td>
+                <td>
+                    <?php $userInfo["wachtwoord"] ?>
+                </td>
+                <td>
+                    <?php $userInfo["created_at"] ?>
+                </td>
+                <td>
+                    <?php $userInfo["updated_at"] ?>
+                </td>
+            </tr>
+        </table>
+    </div>
+<?php } ?>
 </body>
 </html>
 <!-- <?php }else{header('location:/admin');} ?> -->
