e8726864bc8b054296590bfa83f3d41105c38283
Mkirez <m.kirez@hotmail.com>
2020-10-22 17:27:10 +0200

    About page

--- Website/core/config.php
+++ Website/core/config.php
@@ -2,5 +2,5 @@
 
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'PASSWORD');
-define('DB_NAME', 'flevo');
+define('DB_PASSWORD', 'root');
+define('DB_NAME', 'products');
